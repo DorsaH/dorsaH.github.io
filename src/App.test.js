0fdc76a691dd3b655264fcd654d53382a1b1c8b8
Me <dorsah.75@gmail.com>
2022-09-07 15:33:55 +0800

    Created build-test-deploy Github Action

--- src/App.test.js
+++ src/App.test.js
@@ -4,8 +4,8 @@ import App from './App';
 // This test checks to see that a hyperlink is rendered with the expected url and text values
 test('renders link', () => {
   render(<App />);
-  const expectedLinkText = 'From local source code to fully tested and deployed application in a single push';
-  const expectedLinkUrl = 'https://www.redhat.com/en/topics/devops/what-is-ci-cd'
+  const expectedLinkText = 'Who needs automation! Manual deploys for the win!';
+  const expectedLinkUrl = 'https://www.youtube.com/watch?v=oHg5SJYRHA0'
   const linkElement = screen.getByText(expectedLinkText);
   expect(linkElement).toBeInTheDocument();
   expect(linkElement.closest('a')).toHaveAttribute('href', expectedLinkUrl);
